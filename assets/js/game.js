b67ac698997fc96b8f9bba4dade90934d3fb1ec8
Paul Linck <paul.linck@gmail.com>
2018-12-11 14:21:34 -0500

    Small refactoring.

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,7 @@
 "use strict";
 /*global $:false, jQuery:false */
 /*global alert:false */
+/*global document:false */
 
 
 $(document).ready(function () {
@@ -28,7 +29,7 @@ $(document).ready(function () {
     let totalIncorrectAnswers = $("#totalIncorrectAnswers");
 
     // Timers and counters
-    // ====================================================
+    // =============================================================================
     const nextQuestionTimeoutTime = 2000; // Amount of time to display next question
     let nextQuestionTimeoutTimer; // imer variable to allow cancelling timer
     let nextQuestionCountdown; // Countdown to next Question
@@ -38,33 +39,27 @@ $(document).ready(function () {
     let answerCountdown; // to show how much left
 
     // Game tracking
-    // ====================================================
+    // ==============================================================
     let totalCorrectScore = 0; // Total correct answers for the user
     let totalIncorrectScore = 0; // Total correct answers for the user
     let currentQuestionIdx; // the index of question being asking
 
-    // Array of Questio Objects - move this to JSON File
-    // ====================================================
+    // Array of Question Objects - move this to JSON File
+    // ===============================================================
     let myQuestions = [];
+
+    // Custom question to make testing easier
     let customQuestion = {
-        question: "loops?",
-        answers: ["&#39;For&#39; loops", "Bill Clinton", "George HW Bush", "Gerald Ford"],
-        correctAnswer: "&#39;For&#39; loops",
+        question: "Who was the 41st President?",
+        answers: ["George W Bush", "Bill Clinton", "George HW Bush", "Gerald Ford"],
+        correctAnswer: "George HW Bush",
+        correctAnswerIdx: 2,
         correctImage: ""
     };
 
-    // Go to the next question in the list - wrap around if no more
-    function displayNextQuestion() {
-        if (currentQuestionIdx == undefined) {
-            currentQuestionIdx = 0;
-        } else {
-            currentQuestionIdx++;
-        }
-
-        currentQuestion.html(myQuestions[currentQuestionIdx].question);
-    }
-
-    // Helper function to shuffle an array - The answers array has "answer" is always first in the JSON object array
+    // Helper function to shuffle an array 
+    // I may or may not use this but I like the code here for reference
+    // I am very keen on the new ES6 syntac for swapping array items in use below
     function shuffle(arr) {
         // go from last to first element
         for (let i = arr.length - 1; i > 0; i--) {
@@ -81,7 +76,7 @@ $(document).ready(function () {
         return arr;
     }
 
-    // move the correct answer in the array re-assign the correct answer index
+    // Move the correct answer in the array so it isnt always in the same position
     // This does not suffle the wole answer array just randomly assigns where the answer appears in the array
     function moveCorrectAnswer(currentCorrectAnswerIdx, arr) {
         // Using index so I dont have to compare strings with funky characters
@@ -96,11 +91,22 @@ $(document).ready(function () {
         return arr;
     }
 
-    // display the answer choices for the current question
+    // Display the next question in the list - wrap around if no more
+    function displayNextQuestion() {
+        if (currentQuestionIdx == undefined) {
+            currentQuestionIdx = 0;
+        } else {
+            currentQuestionIdx++;
+        }
+
+        currentQuestion.html(myQuestions[currentQuestionIdx].question);
+    }
+
+    // Display the answer choices for the current question
     function displayAnswerChoices() {
         possibleAnswers.empty();
 
-        // let shuffledArray = shuffle(myQuestions[currentQuestionIdx].answers);
+        // Move correct answer in list so its not always in same spot
         let shuffledArray = moveCorrectAnswer(myQuestions[currentQuestionIdx].correctAnswerIdx, myQuestions[currentQuestionIdx].answers);
         for (var i in shuffledArray) {
             var btnChoice = $(`<div class="answer-btn" data-value="${i}">${shuffledArray[i]}</div>`);
@@ -108,19 +114,17 @@ $(document).ready(function () {
         }
     }
 
-    // get next question ready to display in a certain amount of time
+    // Get next question ready to display in a certain amount of time
+    // NOTE: When dealing with timers like intervals and timeouts, its is nice to have a "parent" function
     function setupNextQuestion() {
         // Clear the previous question
         currentQuestion.empty();
         possibleAnswers.html("Waiting for next question ...");
 
         // The end
-        console.log("currentQuestionIdx", currentQuestionIdx);
-
         if (currentQuestionIdx >= (myQuestions.length - 1)) {
             endCurrentGame(totalCorrectScore, totalIncorrectScore);
         } else { // continue game
-            // clear the timeout
             clearTimeout(nextQuestionTimeoutTimer);
             // Wait a few seconds and then display next question
             nextQuestionTimeoutTimer = setTimeout(askAQuestion, nextQuestionTimeoutTime);
@@ -132,7 +136,7 @@ $(document).ready(function () {
 
         answerCountdown -= 1;
 
-        $("#timeRemaining").html(answerCountdown);
+        timeRemaining.html(answerCountdown);
 
         if (answerCountdown <= 0) {
             clearInterval(answerIntervalTimer);
@@ -141,22 +145,20 @@ $(document).ready(function () {
     }
 
     // After answer is given, check to see if user was correct
-    function checkAnswer(answer) {
-        let myAnswerIndex = parseInt(answer, 10);
+    // NOTE: The correct answer is the index of the correct answer in the array vs the text of the answer
+    // It makes it easier to check for correct answer when escaped or unicode characters are mixed in (e.g. &#39;)
+    function checkAnswer(answerIdx) {
+        let myAnswerIndex = parseInt(answerIdx, 10);
         let correctAnswerIndex = myQuestions[currentQuestionIdx].correctAnswerIdx;
-
-        let convertedCorrectAnswer = myQuestions[currentQuestionIdx].correctAnswer;
-        convertedCorrectAnswer = convertedCorrectAnswer.replace(/&#39;/g, " "); // So strings can properly compare with funky characters
-        let convertedAnswer = answer.replace(/'/g, " ");
 
         if (myAnswerIndex === correctAnswerIndex) {
             totalCorrectScore += 1;
             displayLastResult(myQuestions[currentQuestionIdx].answers[myAnswerIndex], "Correct!");
-            alert("Correct!");
+            alert(`${myQuestions[currentQuestionIdx].answers[correctAnswerIndex]} is Correct!`);
         } else {
             totalIncorrectScore += 1;
             displayLastResult(myQuestions[currentQuestionIdx].answers[myAnswerIndex], "Wrong!");
-            alert("Wrong!");
+            alert(`Wrong! Correct answer is: ${myQuestions[currentQuestionIdx].answers[correctAnswerIndex]}`);
         }
 
         // Queue Up Next Question
@@ -226,32 +228,30 @@ $(document).ready(function () {
 
     // Helper HTTP Function for Async calls
     // ===================================================================================================
-    var HttpClientAsync = function () {
-        this.get = function (aUrl, aCallback) {
-            var anHttpRequest = new XMLHttpRequest();
-            anHttpRequest.onreadystatechange = function () {
-                if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                    aCallback(anHttpRequest.responseText);
-            };
-
-            anHttpRequest.open("GET", aUrl, true);
-            anHttpRequest.send(null);
+    function HttpClientAsync(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function () {
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
         };
-    };
+
+        anHttpRequest.open("GET", aUrl, true);
+        anHttpRequest.send(null);
+    }
 
     // Read from JSON URL and convert to list of questions since format is not exactly what I want
     // for exmaple, the possible answers need to include the correct and incorrect answers
-    // this step can be eliminated later by changing the code inside slightly
+    // this step can be eliminated later by changing the code inside slightly but I like it this way
     // ===================================================================================================
     function setupQuestions() {
         var client = new HttpClientAsync();
         let jsonQuestions = {};
 
-        // PUBLIC DOMAIN API TO GET QUESTIONS
+        // PUBLIC DOMAIN API TO GET QUESTIONS e.g.:
         // Computers - https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple
-        // client.get('https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple', function (escaped_response) {
-        client.get('https://opentdb.com/api.php?amount=10&category=18&type=multiple', function (escaped_response) {
-            let response = unescape(escaped_response);
+        // General - https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple
+
+        HttpClientAsync('https://opentdb.com/api.php?amount=10&category=18&type=multiple', function (response) {
 
             jsonQuestions = JSON.parse(response);
             myQuestions.length = 0;
@@ -262,7 +262,6 @@ $(document).ready(function () {
 
                 currentQuestion.question = jsonQuestions.results[i].question;
                 currentQuestion.answers = [jsonQuestions.results[i].correct_answer].concat(jsonQuestions.results[i].incorrect_answers);
-                // currentQuestion.answers = jsonQuestions.results[i].incorrect_answers;
 
                 currentQuestion.correctAnswer = jsonQuestions.results[i].correct_answer;
                 currentQuestion.correctAnswerIdx = 0;
@@ -270,7 +269,8 @@ $(document).ready(function () {
 
                 myQuestions[i] = currentQuestion;
             }
-            // Add my custom question to front of array
+
+            // Add my custom question to front of array for testing
             //myQuestions.unshift(customQuestion);
 
             // Start game by asking a question - note this is async so dont ask until the response comes back
@@ -281,13 +281,14 @@ $(document).ready(function () {
     /****************************************************************************************
      * MAIN
      * The whole thing starts by setting up the questions and asking the first one.
-     * Since I am getting the questions from a server, I am doing it async (no-blocking so
-     * I do  ask the first question inside the callback that returns from the server.
+     * Since I am getting the questions from a server, I am doing it async/non-blocking so
+     * I ask the first question inside the callback that returns from the server.
      * That way, the game will only start when that is completed
      **************************************************************************************** */
     setupQuestions();
 
-    // must chain to parent to get all even on reset
+    // Wait for the user to click on an answer
+    // must chain to parent to get all clicks of .answer-btn since created later - even on reset
     $("#possibleAnswers").on("click", ".answer-btn", function () {
         // get value
         let currentAnswer = $(this).attr("data-value");
@@ -300,6 +301,7 @@ $(document).ready(function () {
         checkAnswer(currentAnswer);
     });
 
+    // Allow user to end the game early
     $("#endGameButton").on("click", function () {
         // clear timer since thet did answer - need to start new timer
         // when you ask the next question
@@ -308,6 +310,4 @@ $(document).ready(function () {
 
     });
 
-
-
-});
+}); // $(document).ready
